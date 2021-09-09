82d00c930432873eef7cea9ce25466105d8c1542
David Bartoš <david.bartos@tietoevry.com>
2021-09-09 12:34:00 +0200

    update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,5 @@ terraform {
     storage_account_name = "tstate12399"
     container_name       = "tstate-mondev"
     key                  = "terraform.tfstate"
-    access_key           = "chbeYCgEGXeb5lhr4gsK4bX0F82MTUSt3pyDTjONiB4MiaUx+6qV/xeIrlkrYXbaTzVQY43CLNI/VBomvigZCA=="
   }
 }
