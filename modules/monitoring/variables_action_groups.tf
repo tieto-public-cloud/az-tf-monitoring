a63bbe13756dfb668ca45eca70ae771d416f4eb0
David Bartoš <david.bartos@tietoevry.com>
2021-10-21 20:00:02 +0200

    Enable development Service Now endpoint for testing event integrations with new transform script

--- modules/monitoring/variables_action_groups.tf
+++ modules/monitoring/variables_action_groups.tf
@@ -9,7 +9,7 @@ variable "action_groups" {
       }
       webhook = {
         name                    = "dev"
-        service_uri             = "https://eneg9df92ti3jgb.m.pipedream.net"
+        service_uri             = "https://EventManagement_TestingIntegration:Ostrava123456@tietodev1.service-now.com/api/global/em/inbound_event?source=AzureGCP"
         use_common_alert_schema = true
       }
     }
@@ -21,7 +21,7 @@ variable "action_groups" {
       }
       webhook = {
         name                    = "dev"
-        service_uri             = "https://eneg9df92ti3jgb.m.pipedream.net"
+        service_uri             = "https://EventManagement_TestingIntegration:Ostrava123456@tietodev1.service-now.com/api/global/em/inbound_event?source=AzureGCP"
         use_common_alert_schema = true
       }
     }
